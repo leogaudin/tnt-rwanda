261c02d5acd194ae9a76fb81032f53619cd22ac3
Léo <leogaudinoff@gmail.com>
2024-04-26 09:25:15 +0200

    build: change bundle identifiers

--- application/android/app/src/main/java/com/leogaudin/tnt/MainActivity.java
+++ application/android/app/src/main/java/com/leogaudin/tnt/MainActivity.java
@@ -1,4 +1,4 @@
-package com.gaudin.tntmalawi;
+package com.gaudin.tntrwanda;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
--- application/android/app/src/main/java/com/leogaudin/tnt/MainApplication.java
+++ application/android/app/src/main/java/com/leogaudin/tnt/MainApplication.java
@@ -1,4 +1,4 @@
-package com.gaudin.tntmalawi;
+package com.gaudin.tntrwanda;
 
 import android.app.Application;
 import com.facebook.react.PackageList;
