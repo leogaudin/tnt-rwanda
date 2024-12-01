065ed02b38049857e10bcd89fd619704011109ff
Léo <leogaudinoff@gmail.com>
2024-12-01 15:08:35 +0100

    fix: missing import

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -4,9 +4,11 @@ import Insights from './components/Insights';
 import { useContext } from 'react';
 import AppContext from '../../context';
 import Loading from '../../components/Loading';
+import { useTranslation } from 'react-i18next';
 
 export default function Home() {
 	const { insights } = useContext(AppContext);
+	const { t } = useTranslation();
 
 	if (!insights)
 		return <Loading message={t('insightsLoading')} />;
