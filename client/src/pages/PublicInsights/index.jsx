e4b649de982ef7574dbd2068b549b910c9d49b97
Léo <leogaudinoff@gmail.com>
2025-03-25 19:31:18 +0100

    feat: add possibility for single project sharing

--- client/src/pages/PublicInsights/index.jsx
+++ client/src/pages/PublicInsights/index.jsx
@@ -1,29 +1,61 @@
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useSearchParams } from 'react-router-dom';
 import { fetchInsights } from '../../service';
 import Insights from '../Home/components/Insights';
 import BigLoading from '../../components/BoxesLoading';
+import ProjectInsights from '../Home/components/ProjectInsights';
+import { computeInsights } from '../../service/stats';
+import NothingToSee from '../../components/NothingToSee';
 
 export default function PublicInsights() {
 	const { id } = useParams();
+	const [searchParams] = useSearchParams();
+	const project = searchParams.get('project');
 	const [rawInsights, setRawInsights] = useState(null);
+	const [projectInsights, setProjectInsights] = useState(null);
+	const [nothingToSee, setNothingToSee] = useState(false);
 
 	useEffect(() => {
-		fetchInsights({ adminId: id })
+		fetchInsights({
+			adminId: id,
+			...(project ? { project } : {})
+		})
 			.then((raw) => {
-				setRawInsights(raw);
+				if (!raw || raw.length === 0) {
+					return setNothingToSee(true);
+				}
+				if (project) {
+					setProjectInsights(raw);
+				} else {
+					setRawInsights(raw);
+				}
 			})
 			.catch((e) => console.error(e))
 	}, [id])
 
+	if (nothingToSee) {
+		return <NothingToSee />
+	}
+
+	if (!rawInsights && !projectInsights) {
+		return <BigLoading />
+	}
+
 	return (
 		<>
-			{rawInsights
-				? <Insights
+			{projectInsights &&
+				<ProjectInsights
+					insights={computeInsights(projectInsights, { grouped: false })}
+					project={project}
+					menu={null}
+					id={id}
+				/>
+			}
+			{rawInsights &&
+				<Insights
 					rawInsights={rawInsights}
 					id={id}
 				/>
-				: <BigLoading />
 			}
 		</>
 	);
